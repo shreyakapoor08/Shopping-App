9de2c5143a2b94e3893ac8a454aff9df01ea6e6d
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-25 17:46:19 +0530

    working on product.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,11 +3,26 @@ const route =  require('express').Router();
 
 route.get('/', (req, res) => {
     //get all products
+    Product.findAll()
+        .then((products) => {
+            res.status(200).send(products)
+        })
+        .catch((err) => {
+            res.status(500),send({
+                error: "Could not retrieve products"
+            })
+        })
 
 })
 
 route.post('/', (req, res)  => {
     //Add a new product
+    Product.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price: parseFloat(req.body.price) //in post request everything goes as a string
+    })
+
 
 })
 
