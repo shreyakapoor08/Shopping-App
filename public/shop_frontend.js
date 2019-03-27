8a1f17422c9a60191ab82b7d815318577a791873
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-27 10:41:40 +0530

    shop_frontend.js started

---
+++ public/shop_frontend.js
@@ -0,0 +1,28 @@
+
+function fetchProducts (done) {
+    $.get('/api/products', function (data) {
+        done(data) //this data is all the data that we have fetched
+    })
+}
+
+function createProductCard (product) {
+    return $(` 
+    <div class="col-4 card mx-2 p-4">
+       <h4 class="product-name">${product.name}</h4>
+       <div class="product-manufacturer">${product.manufacturer}</div>
+       <div class="row">
+       <div class="col m-3 p-3">
+       <b>Rs. ${product.price}</b>
+       </div>
+       <button class="col btn btn-primary m-3">Buy</button>
+       </div>
+    </div> `
+    )
+}
+
+$(function () {
+
+    let productList = $('#product-list')
+})
+
+//we will make our function into our dollar function which makes it load after the window is loaded
